ec8c0912d11199bb32cd3cf22c6fa6d2b5ffa1eb
minuteness <82571955+minuteness@users.noreply.github.com>
2021-04-21 14:48:04 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,64 @@
+window.onload = function(){
+    document.getElementById('menu').innerHTML =
+	'<div class="left">
+  <ul class="nav">
+     <li>
+	   <a href="#1"><span class="menu-title">ФИЗИКА</span></a>
+	      <ul>
+			   <li><a href="physics-1.html"><span class="menu-title">номер 1 номер 1 номер 1 номер 1 номер 1</span></a></li>
+			   <li><a href="physics-2.html"><span class="menu-title">номер 2</span></a></li>
+			   <li><a href="physics-3.html"><span class="menu-title">номер 3</span></a></li>
+			   <li><a href="physics-4.html"><span class="menu-title">номер 4</span></a></li>
+		   </ul>
+	 </li>
+     <li>
+	  <a href="#2"><span class="menu-title">ХИМИЯ</span></a>
+	       <ul>
+			   <li><a href="chemistry-1.html"><span class="menu-title">номер 1</span></a></li>
+			   <li><a href="chemistry-2.html"><span class="menu-title">номер 2</span></a></li>
+		   </ul>
+	 </li>
+     <li>
+	  <a href="#3"><span class="menu-title">БИОЛОГИЯ</span></a>
+	       <ul>
+			   <li><a href="biology-1.html"><span class="menu-title">номер 1</span></a></li>
+			   <li><a href="biology-2.html"><span class="menu-title">номер 2</span></a></li>
+			   <li><a href="biology-3.html"><span class="menu-title">номер 3</span></a></li>
+			   <li><a href="biology-4.html"><span class="menu-title">номер 4</span></a></li>
+		   </ul>
+	 </li>
+     <li>
+	  <a href="obzh.html"><span class="menu-title">ОБЖ</span></a>
+	 </li>
+     <li>
+	  <a href="#5"><span class="menu-title">МАТЕМАТИКА</span></a>
+	       <ul>
+			   <li><a href="mathematics-1.html"><span class="menu-title">номер 1</span></a></li>
+			   <li><a href="mathematics-2.html"><span class="menu-title">номер 2</span></a></li>
+			   <li><a href="mathematics-3.html"><span class="menu-title">номер 3</span></a></li>
+			   <li><a href="mathematics-4.html"><span class="menu-title">номер 4</span></a></li>
+			   <li><a href="mathematics-5.html"><span class="menu-title">номер 5</span></a></li>
+		   </ul>
+	 </li>
+	  <li>
+	  <a href="#6"><span class="menu-title">АСТРОНОМИЯ</span></a>
+	      <ul>
+			   <li><a href="astronomy-1.html"><span class="menu-title">номер 1</span></a></li>
+			   <li><a href="astronomy-2.html"><span class="menu-title">номер 2</span></a></li>
+			   <li><a href="astronomy-3.html"><span class="menu-title">номер 3</span></a></li>
+			   <li><a href="astronomy-4.html"><span class="menu-title">номер 4</span></a></li>
+			   <li><a href="astronomy-5.html"><span class="menu-title">номер 5</span></a></li>
+		   </ul>
+	 </li>
+	  <li>
+	  <a href="#7"><span class="menu-title">ГЕОГРАФИЯ</span></a>
+	       <ul>
+			   <li><a href="geography-1.html"><span class="menu-title">номер 1</span></a></li>
+			   <li><a href="geography-2.html"><span class="menu-title">номер 2</span></a></li>
+			   <li><a href="geography-3.html"><span class="menu-title">номер 3</span></a></li>
+			   <li><a href="geography-4.html"><span class="menu-title">номер 4</span></a></li>
+		   </ul>
+	 </li>
+    </ul> 
+</div>';
+};
